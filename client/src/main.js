2fa77e8654b14e6b3a83304106f07911bddef3cf
matekelemen <kelemen.mate.12@gmail.com>
2023-02-13 00:28:35 +0100

    remove comments

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,5 @@
 import {createApp} from "vue";
 import App from "./App.vue";
-//import EmbeddedTerminal from "./components/EmbeddedTerminal.vue";
 
 const app = createApp(App)
-//app.component("EmbeddedTerminal", EmbeddedTerminal);
 app.mount("#app")
